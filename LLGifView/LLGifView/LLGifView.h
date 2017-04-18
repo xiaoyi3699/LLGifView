5fd188602a9d864de5fc80bc6be37b015465e5f5
WangZhaomeng <wangzhaomeng@wangzhaomengdemacbook-pro.local>
2017-04-18 13:45:36 +0800

    动画添加缓存处理

--- LLGifView/LLGifView/LLGifView.h
+++ LLGifView/LLGifView/LLGifView.h
@@ -7,11 +7,30 @@
 //
 
 #import <UIKit/UIKit.h>
-@interface LLGifView : UIView 
+@interface LLGifView : UIView
 
 - (id)initWithFrame:(CGRect)frame filePath:(NSString *)filePath;
 - (id)initWithFrame:(CGRect)frame data:(NSData *)data;
+- (id)initWithFrame:(CGRect)frame url:(NSString *)url;
+
+/**
+ 开始动画
+ */
 - (void)startGif;
+
+/**
+ 停止并移除gif动画
+ */
 - (void)stopGif;
+
+/**
+ 暂停gif动画
+ */
+- (void)pauseGif;
+
+/**
+ 恢复gif动画
+ */
+- (void)resumeGif;
 
 @end
--- LLGifView/LLGifView/LLGifView.m
+++ LLGifView/LLGifView/LLGifView.m
@@ -9,12 +9,19 @@
 #import "LLGifView.h"
 #import <ImageIO/ImageIO.h>
 
+typedef enum {
+    LLGifViewTypeFilePath = 0,
+    LLGifViewTypeData,
+    LLGifViewTypeUrl
+} LLGifViewType;
+#define LLGifAnimationKey @"LL_GifAnimation"
 @interface LLGifView (){
-    CGImageSourceRef    _gif;
-    NSInteger           _index;
-    NSInteger           _count;
-    NSTimer             *_timer;
-    NSDictionary        *_gifProperties;
+    LLGifViewType       _type;
+    NSString            *_filePath;
+    NSData              *_data;
+    NSString            *_url;
+    CGImageSourceRef    _imageSourceRef;
+    CAKeyframeAnimation *_animation;
 }
 @end
 
@@ -23,11 +30,8 @@
 - (id)initWithFrame:(CGRect)frame filePath:(NSString *)filePath{
     self = [super initWithFrame:frame];
     if (self) {
-        NSDictionary *dic = @{(NSString *)kCGImagePropertyGIFLoopCount:@0};
-        _gifProperties = @{(NSString *)kCGImagePropertyGIFDictionary:dic};
-        _gif = CGImageSourceCreateWithURL((CFURLRef)[NSURL fileURLWithPath:filePath], (CFDictionaryRef)_gifProperties);
-        _count = CGImageSourceGetCount(_gif);
-        [self play];
+        _type     = LLGifViewTypeFilePath;
+        _filePath = filePath;
     }
     return self;
 }
@@ -35,35 +39,135 @@
 - (id)initWithFrame:(CGRect)frame data:(NSData *)data{
     self = [super initWithFrame:frame];
     if (self) {
-        NSDictionary *dic = @{(NSString *)kCGImagePropertyGIFLoopCount:@0};
-        _gifProperties = @{(NSString *)kCGImagePropertyGIFDictionary:dic};
-        _gif = CGImageSourceCreateWithData((CFDataRef)data, (CFDictionaryRef)_gifProperties);
-        _count = CGImageSourceGetCount(_gif);
-        [self play];
+        _type = LLGifViewTypeData;
+        _data = data;
     }
     return self;
 }
 
+- (id)initWithFrame:(CGRect)frame url:(NSString *)url{
+    self = [super initWithFrame:frame];
+    if (self) {
+        _type = LLGifViewTypeUrl;
+        _url = url;
+    }
+    return self;
+}
+
+//开始gif动画
 - (void)startGif{
-    if (!_timer) {
-        _timer = [NSTimer scheduledTimerWithTimeInterval:.1 target:self selector:@selector(play) userInfo:nil repeats:YES];
+    if (_animation) {
+        if ([self.layer animationForKey:LLGifAnimationKey]) {
+            self.layer.speed = 1.0;
+            self.layer.timeOffset = 0.0;
+            self.layer.beginTime = 0.0;
+        }
+        else {
+            [self.layer addAnimation:_animation forKey:LLGifAnimationKey];
+        }
+    }
+    else {
+        dispatch_async(dispatch_get_global_queue(0, 0), ^{
+            if (_type == LLGifViewTypeFilePath) {
+                if ([[NSFileManager defaultManager] fileExistsAtPath:_filePath]) {
+                    NSDictionary     *dic = @{(NSString *)kCGImagePropertyGIFLoopCount:@0};
+                    NSDictionary     *properties = @{(NSString *)kCGImagePropertyGIFDictionary:dic};
+                    _imageSourceRef = CGImageSourceCreateWithURL((CFURLRef)[NSURL fileURLWithPath:_filePath], (CFDictionaryRef)properties);
+                }
+                else {
+                    NSLog(@"gif路径错误");
+                    return;
+                }
+            }
+            else if (_type == LLGifViewTypeData) {
+                NSDictionary        *dic = @{(NSString *)kCGImagePropertyGIFLoopCount:@0};
+                NSDictionary *properties = @{(__bridge_transfer NSString *)kCGImagePropertyGIFDictionary:dic};
+                _imageSourceRef          = CGImageSourceCreateWithData((CFDataRef)_data, (CFDictionaryRef)properties);
+            }
+            else {
+                NSError *error = nil;
+                NSData *data             = [NSData dataWithContentsOfURL:[NSURL URLWithString:_url] options:NSDataReadingUncached error:&error];
+                if (data) {
+                    NSDictionary *dic        = @{(NSString *)kCGImagePropertyGIFLoopCount:@0};
+                    NSDictionary *properties = @{(__bridge_transfer NSString *)kCGImagePropertyGIFDictionary:dic};
+                    _imageSourceRef          = CGImageSourceCreateWithData((CFDataRef)data, (CFDictionaryRef)properties);
+                }
+                else {
+                    NSLog(@"gif加载出错:\n url = [%@] \n error = [%@]",_url,error);
+                }
+            }
+            
+            if (_imageSourceRef) {
+                size_t imageCount        = CGImageSourceGetCount(_imageSourceRef);
+                NSMutableArray *images   = [[NSMutableArray alloc] initWithCapacity:imageCount];
+                NSMutableArray *times    = [[NSMutableArray alloc] initWithCapacity:imageCount];
+                NSMutableArray *keyTimes = [[NSMutableArray alloc] initWithCapacity:imageCount];
+                CGFloat totalTime = 0;
+                
+                for (size_t i = 0; i < imageCount; i++) {
+                    CGImageRef cgimage = CGImageSourceCreateImageAtIndex(_imageSourceRef, i, NULL);
+                    [images addObject:(__bridge id)cgimage];
+                    CFRelease(cgimage);
+                    
+                    NSDictionary *properties    = (__bridge_transfer NSDictionary *)CGImageSourceCopyPropertiesAtIndex(_imageSourceRef, i, NULL);
+                    NSDictionary *gifProperties = [properties valueForKey:(__bridge NSString *)kCGImagePropertyGIFDictionary];
+                    NSString *gifDelayTime      = [gifProperties valueForKey:(__bridge NSString*)kCGImagePropertyGIFDelayTime];
+                    [times addObject:gifDelayTime];
+                    totalTime += [gifDelayTime floatValue];
+                }
+                CFRelease(_imageSourceRef);
+                
+                float currentTime = 0;
+                for (size_t i = 0; i < times.count; i++) {
+                    float keyTime = currentTime / totalTime;
+                    [keyTimes addObject:[NSNumber numberWithFloat:keyTime]];
+                    currentTime += [[times objectAtIndex:i] floatValue];
+                }
+                
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    _animation = [CAKeyframeAnimation animationWithKeyPath:@"contents"];
+                    [_animation setTimingFunction:[CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionLinear]];
+                    [_animation setValues:images];
+                    [_animation setKeyTimes:keyTimes];
+                    _animation.duration = totalTime;
+                    _animation.removedOnCompletion = NO;
+                    _animation.fillMode = kCAFillModeForwards;
+                    _animation.repeatCount = HUGE_VALF;
+                    [self.layer addAnimation:_animation forKey:LLGifAnimationKey];
+                });
+            }
+            else {
+                NSLog(@"未识别的gif图片");
+            }
+        });
     }
 }
 
+//停止并移除gif动画
 - (void)stopGif{
-    if (_timer) {
-        [_timer invalidate];
-        _timer = nil;
+    if ([self.layer animationForKey:LLGifAnimationKey]) {
+        [self.layer removeAnimationForKey:LLGifAnimationKey];
     }
 }
 
-- (void)play{
-    if (_gif) {
-        _index = _index%_count;
-        CGImageRef ref = CGImageSourceCreateImageAtIndex(_gif, _index, (CFDictionaryRef)_gifProperties);
-        self.layer.contents = (__bridge id)ref;
-        CFRelease(ref);
-        _index ++;
+//暂停gif动画
+- (void)pauseGif{
+    if ([self.layer animationForKey:LLGifAnimationKey]) {
+        CFTimeInterval pausedTime = [self.layer convertTime:CACurrentMediaTime() fromLayer:nil];
+        self.layer.speed = 0.0;
+        self.layer.timeOffset = pausedTime;
+    }
+}
+
+//恢复gif动画
+- (void)resumeGif{
+    if ([self.layer animationForKey:LLGifAnimationKey]) {
+        CFTimeInterval pausedTime = [self.layer timeOffset];
+        self.layer.speed = 1.0;
+        self.layer.timeOffset = 0.0;
+        self.layer.beginTime = 0.0;
+        CFTimeInterval timeSincePause = [self.layer convertTime:CACurrentMediaTime() fromLayer:nil] - pausedTime;
+        self.layer.beginTime = timeSincePause;
     }
 }
 
@@ -73,8 +177,7 @@
 }
 
 - (void)dealloc {
-    NSLog(@"gifView释放，无内存泄漏");
-    CFRelease(_gif);
+    NSLog(@"释放");
 }
 
 @end
